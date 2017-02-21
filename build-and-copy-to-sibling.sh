d688dabce01c026847104a465a2d0d4c361064a5
Stephan Druskat <mail@sdruskat.net>
2017-02-21 13:13:38 +0100

    Change script to copy to root in gh-pages

--- build-and-copy-to-sibling.sh
+++ build-and-copy-to-sibling.sh
@@ -3,6 +3,6 @@ echo "Building P2 site."
 mvn p2:site
 read -p "Enter directory where 'corpus-tools/p2' has been cloned to (must be on branch 'gh-pages'): " -e -i ../corpus-tools-p2 DIR
 echo "Moving target to $DIR."
-mv ./target/* $DIR
+mv ./target/repository/* $DIR
 rm -r ./target
 echo "Done."
