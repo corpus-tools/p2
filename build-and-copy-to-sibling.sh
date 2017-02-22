2a372becbea766346e122373c5ca6d8c211a2477
Stephan Druskat <mail@sdruskat.net>
2017-02-22 09:52:45 +0100

    Include exit code check for mvn

--- build-and-copy-to-sibling.sh
+++ build-and-copy-to-sibling.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "Building P2 site."
 mvn clean p2:site
+rc=$?
+if [[ $rc -ne 0 ]] ; then
+  echo 'Maven build failed. Exiting now.'; exit $rc
+fi
 read -p "Enter directory where 'corpus-tools/p2' has been cloned to (must be on branch 'gh-pages'): " -e -i ../corpus-tools-p2 DIR
 echo "Moving target to $DIR."
 mv ./target/repository/* $DIR
 rm -r ./target
 echo "Done."
+exit 0
